a9dfcfb81f7e49dd1860d7eaea212790067167f1
Mac Gi <maevyboy@googlemail.com>
2013-01-14 23:01:03 +0100

    Changed getting messages in SQSUtil so it gets now alle available messages

--- SI_Download_Service/src/de/fhb/dlService/SqsUtil.java
+++ SI_Download_Service/src/de/fhb/dlService/SqsUtil.java
@@ -1,5 +1,6 @@
 package de.fhb.dlService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.amazonaws.services.sqs.AmazonSQS;
@@ -11,7 +12,6 @@ import com.amazonaws.services.sqs.model.Message;
 import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
 
-
 /**
  * This class is a both entrypoint utility class which manages writing and reading out of AWS SQS
  * @author MaccaPC
@@ -21,66 +21,82 @@ import com.amazonaws.services.sqs.model.SendMessageRequest;
 public class SqsUtil {
     /** The aws sqs credentials instance*/
     public AmazonSQS sqs = null;
+
     /**
      * The constructor
      * @param aSqs the sqs instance
      */
-    public SqsUtil(AmazonSQS aSqs){
+    public SqsUtil(AmazonSQS aSqs) {
         this.sqs = aSqs;
     }
+
     /**
      * Creates a SQS Queue.
      * @param aQueueName the queuename
      * @return the queueUrl
      */
-    public String createQueue(String aQueueName){
+    public String createQueue(String aQueueName) {
         CreateQueueRequest qRequest = new CreateQueueRequest(aQueueName);
         String aQueueUrl = sqs.createQueue(qRequest).getQueueUrl();
         return aQueueUrl;
     }
+
     /**
      * sends a Message to a named Queue
      * @param aQueueName
      * @param aMessage
      */
-    public void sendSqsMessage(String aQueueName, String aMessage){
-       String aQueueUrl = getQueueUrl(aQueueName);
-       sqs.sendMessage(new SendMessageRequest(aQueueUrl, aMessage)); 
+    public void sendSqsMessage(String aQueueName, String aMessage) {
+        String aQueueUrl = getQueueUrl(aQueueName);
+        sqs.sendMessage(new SendMessageRequest(aQueueUrl, aMessage));
     }
+
     /**
      * Gets the message from a named queue
      * @param aQueueName
      * @return a list containing all received messages
      */
-    public List<Message> getMessagefromAnotherEntrypoint(String aQueueName){
+    public ArrayList<Message> getMessagefromAnotherEntrypoint(String aQueueName) {
+        ReceiveMessageRequest aMessageRequest;
+        String queueUrl = getQueueUrl(aQueueName);
+        ArrayList<Message> resultMessagePoll = new ArrayList<Message>();
+        List<Message> deleteMessage = null;
         try {
-            String queueUrl = getQueueUrl(aQueueName);            
-            ReceiveMessageRequest aMessageRequest = new ReceiveMessageRequest(queueUrl);
-            List<Message> messages = sqs.receiveMessage(aMessageRequest).getMessages();
-            return messages;
-            
+            do {
+                aMessageRequest = new ReceiveMessageRequest(queueUrl).withMaxNumberOfMessages(10);
+                deleteMessage = sqs.receiveMessage(aMessageRequest).getMessages();
+                resultMessagePoll.addAll(deleteMessage);
+                for (Message message : deleteMessage) {
+                    deleteMessageAfterRead(aQueueName, message);
+                }
+            } while (deleteMessage.size() != 0);
+
+            return resultMessagePoll;
+
         } catch (Exception e) {
             // do nothing
         }
         return null;
     }
+
     /**
      * Gets the queueUrl
      * @param aQueueName the given queue name
-     * @return
+     * @return the queueUrl
      */
-    private String getQueueUrl(String aQueueName) {
+    public String getQueueUrl(String aQueueName) {
         GetQueueUrlRequest queueUrlRequest = new GetQueueUrlRequest(aQueueName);
         GetQueueUrlResult queueUrlResult = sqs.getQueueUrl(queueUrlRequest);
         String queueUrl = queueUrlResult.getQueueUrl();
         return queueUrl;
     }
+
     /**
      * Deletes designated Message
      * @param aQueueName the queue name
      * @param aMessage the message
      */
-    public void deleteMessageAfterRead(String aQueueName, Message aMessage){
+    public void deleteMessageAfterRead(String aQueueName, Message aMessage) {
         String aQueueUrl = getQueueUrl(aQueueName);
         sqs.deleteMessage(new DeleteMessageRequest(aQueueUrl, aMessage.getReceiptHandle()));
     }
